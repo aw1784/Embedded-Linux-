404a23944d02c6c3389e6febcf0aee3c2ae14729
aw1784 <ahmedwalid1784@gmail.com>
2024-06-23 21:30:53 +0300

    lec1 Task Solution v1

--- Python/Lec1/task5.py
+++ Python/Lec1/task5.py
@@ -1,7 +1,7 @@
 import os
+print("\n*************************\n")
 print(os.environ)
 print("\n*************************\n")
 print(os.environ['HOME'])
 print("\n*************************\n")
 print(os.environ['PATH' ])
-print("\n*************************\n")
