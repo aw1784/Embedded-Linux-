d92ff0b4960dd067e7a6c20bbabbd302ca35611c
Ahmed Walid <ahmedwalid1784@gmail.com>
2024-06-27 16:44:03 +0300

    Alexa Task Solution

---
+++ Python/Lec2/Alexa.py
@@ -0,0 +1,92 @@
+from gtts import gTTS
+import vlc
+import time
+import speech_recognition as sr
+import webbrowser
+from datetime import datetime
+from datetime import date
+
+
+def voice(responce,sleep_time):
+        x= gTTS(
+            text= responce,lang='en',slow=False
+        )
+        x.save("welcome.mp4")
+        p=vlc.MediaPlayer("./welcome.mp4")
+        p.play()
+        while True:
+            pass
+            time.sleep(int(sleep_time))
+            break
+response= 'Welcome How can I help you ?'
+voice(response,3)        
+
+def get_voice_command():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening for your command...")
+        audio = recognizer.listen(source)
+        try:
+            command = recognizer.recognize_google(audio)
+            print(f"You said: {command}")
+            return command.lower()
+        except sr.UnknownValueError:
+            responce= "Sorry, I could not understand your command."
+            voice(responce,3)
+            return ""
+        except sr.RequestError:
+            print("Could not request results from Google Speech Recognition service.")
+            return ""
+
+def tell_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
+
+
+def main():
+    
+    while True:
+        command = get_voice_command()
+
+        if 'time' in command:
+            current_time = tell_time()
+            response = f"The current time is {current_time}."
+            voice(response,5)
+
+        elif 'google' in command:
+            webbrowser.open("https://www.google.com")
+            response = "Opening Google."
+            voice(response,2)
+        elif 'matches today' in command:
+            today = date.today()
+            today= str(today).split("-")
+            today=today[::-1]
+            today=f"{today[1]}/{today[0]}/{today[2]}"
+            print(today)
+            webbrowser.open(f"https://www.yallakora.com/match-center/%D9%85%D8%B1%D9%83%D8%B2-%D8%A7%D9%84%D9%85%D8%A8%D8%A7%D8%B1%D9%8A%D8%A7%D8%AA?date={today}#")
+            response = "Today matches"
+            voice(response,2)
+        
+        elif 'exit' in command:
+            response = "I hope I have helped you, thank you for using me "
+            voice(response,4)
+
+            break
+
+        else:
+            response = "I did not understand that command."
+            x= gTTS(
+            text= response,lang='en',slow=False
+            )   
+            x.save("welcome.mp4")
+            p=vlc.MediaPlayer("./welcome.mp4")
+            p.play()
+            while True:
+                pass
+                time.sleep(5)
+                break
+
+
+if __name__ == "__main__":
+    main()
