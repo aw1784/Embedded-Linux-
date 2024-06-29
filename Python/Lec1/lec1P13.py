6c0022a595da5d607b44dfbc3af080f46b3b92a3
Ahmed Walid <ahmedwalid1784@gmail.com>
2024-06-30 01:51:54 +0300

    Quick Tasks of Lec 1 Added

---
+++ Python/Lec1/lec1P13.py
@@ -0,0 +1,13 @@
+"""
+    
+   Write a python code that print your information.
+    Full Name, Birth Year, Faculty, E-mail, Address 
+    
+"""
+print (
+    "Name: Ahmed Walid Hassan Abdelanby\n\
+Birth Year: 2003\n\
+Faculty: Faculty of Engineering Alexandria University\n\
+E-mail:ahmedwalid1784@gmail.com\n\
+Address : Maiami / Alexandria / Egypt"
+    )
---
+++ Python/Lec1/lec1P18.py
@@ -0,0 +1,15 @@
+"""
+    Print sum of two numbers
+    Print sub of two numbers
+    Print Div of two numbers
+    Print Mult of two numbers
+    
+"""
+def Calc(x,y):
+    print(f"{x} + {y} = {x+y}")
+    print(f"{x} - {y} = {x-y}")
+    print(f"{x} * {y} = {x*y}")
+    print(f"{x} / {y} = {x/y}")
+x=12
+y=6
+Calc(x,y)
